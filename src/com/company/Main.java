a6823c015295cd9d1df5a7f73f9a70d48e7e10a9
Nasty-Khropik <6159874@gmail.com>
2022-02-02 23:49:04 +0300

    load

---
+++ src/com/company/Main.java
@@ -0,0 +1,132 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte a=1;
+	short b=2;
+	int c=3;
+	long d=4;
+	float i=3.14f;
+	double f= 2.222;
+	char h='M';
+	boolean n=true;
+	String m="Good day";
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(i);
+	System.out.println(f);
+	System.out.println(h);
+	System.out.println(n);
+	System.out.println(m);
+
+	int a1=a+1;
+	System.out.println(a1);
+	int a2=a-1;
+	System.out.println(a2);
+	int a3=a*2;
+	System.out.println(a3);
+	int a4=a/1;
+	System.out.println(a4);
+	int a5=a%1;
+	System.out.println(a5);
+	int a6=a;
+	a6++;
+	System.out.println(a6);
+	int a7=a;
+	a7--;
+	System.out.println(a7);
+
+		int b1=b+1;
+		System.out.println(b1);
+		int b2=b-1;
+		System.out.println(b2);
+		int b3=b*2;
+		System.out.println(b3);
+		int b4=b/2;
+		System.out.println(b4);
+		int b5=b%1;
+		System.out.println(b5);
+		int b6=b;
+		b6++;
+		System.out.println(b6);
+		int b7=b;
+		b7--;
+		System.out.println(b7);
+
+		int c1=c+2;
+		System.out.println(c1);
+		int c2=c-1;
+		System.out.println(c2);
+		int c3=c*3;
+		System.out.println(c3);
+		int c4=c/1;
+		System.out.println(c4);
+		int c5=c%2;
+		System.out.println(c5);
+		int c6=c;
+		c6++;
+		System.out.println(c6);
+		int c7=c;
+		c7--;
+		System.out.println(c7);
+
+		long d1=d+3;
+		System.out.println(d1);
+		long d2=d-2;
+		System.out.println(d2);
+		long d3=d*3;
+		System.out.println(d3);
+		long d4=d/2;
+		System.out.println(d4);
+		long d5=d%3;
+		System.out.println(d5);
+		long d6=d;
+		d6++;
+		System.out.println(d6);
+		long d7=d;
+		d7--;
+		System.out.println(d7);
+
+		double i1=i+1.00;
+		System.out.println(i1);
+		double i2=i-1.04;
+		System.out.println(i2);
+		double i3=i*2.1;
+		System.out.println(i3);
+		double i4=i/0.314;
+		System.out.println(i4);
+		double i5=i%1.5;
+		System.out.println(i5);
+		double i6=i;
+		i6++;
+		System.out.println(i6);
+		double i7=i;
+		i7--;
+		System.out.println(i7);
+
+		double f1=f+1;
+		System.out.println(f1);
+		double f2=f-1.04;
+		System.out.println(f2);
+		double f3=f*2.1;
+		System.out.println(f3);
+		double f4=f/10;
+		System.out.println(f4);
+		double f5=f%1.5;
+		System.out.println(f5);
+		double f6=f;
+		f6++;
+		System.out.println(f6);
+		double f7=f;
+		f7--;
+		System.out.println(f7);
+
+
+
+
+
+    }
+}
