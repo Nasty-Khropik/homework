7010a2977dd12fdb7fe7f693d151a3de18b3ec90
Nasty-Khropik <6159874@gmail.com>
2022-02-18 01:31:53 +0300

    Task for week 3

---
+++ src/com/company/week3/Task1.java
@@ -0,0 +1,21 @@
+package com.company.week3;
+import  java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[5];
+        int i;
+        for (i = 0; i < 5; i++) {
+            System.out.println("Введите любое целое число: ");
+            int num = scan.nextInt();
+            array[i] = num;
+        }
+        for ( i = 0; i < 5; i++) {
+           if (array[i] % 2 == 0)
+               System.out.println("Число " + array[i] + " - четное");
+           else
+               System.out.println("Число " +array[i] + " - нечетное");
+
+       }
+    }
+}
