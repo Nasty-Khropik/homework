6ed2288ad7cf16e72f48cd4f4fff686825d7ac88
AlehShyliuk <47559090+AlehShyliuk@users.noreply.github.com>
2022-02-21 17:20:30 +0400

    Feedback

--- src/com/company/week3/Task2.java
+++ src/com/company/week3/Task2.java
@@ -11,6 +11,10 @@ public class Task2 {
         for (int i = 0; i < result.length; i++) {
             result[i] += num;
         }
+        
+        //здесь не совсем правильно используешь метод copyValueOf. Он должен вызываться не у переменной типа String, 
+        // а у самого класса String (пока это может быть не совсем понятно, но наследующих неделях станет понятнее)
+        // То есть должно выглядеть так: String s1 = String.copyValueOf(result);
         String s1 = "Закодированное слово";
         s1 = s1.copyValueOf(result);
         String str = String.format("Исходное сообщение: %s, зашифрованное сообщение: %s, шифр: %d", word, s1, num);
