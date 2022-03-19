015f872e07b30e02e092a674fdbfda4bd12baed3
Nasty-Khropik <6159874@gmail.com>
2022-03-20 02:33:40 +0300

    Feature:создали класс User и добавили в него поля

---
+++ src/com/company/week7/User.java
@@ -0,0 +1,17 @@
+package com.company.week7;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+
+public class User {
+    String firstName;
+    String lastName;
+    Date birthDate = new Date();
+    boolean gender;
+    String email;
+    String password;
+    ArrayList<String> loan = new ArrayList<String>();
+    ArrayList<String> debitCard = new ArrayList<String>();
+
+}
