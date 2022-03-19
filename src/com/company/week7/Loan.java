f2e5427fe1cd89851dc82ad94313b62bd09b4c5b
Nasty-Khropik <6159874@gmail.com>
2022-03-20 02:44:33 +0300

    Feature:создали класс Loan и добавили в него поля

---
+++ src/com/company/week7/Loan.java
@@ -0,0 +1,12 @@
+package com.company.week7;
+
+import java.util.Date;
+
+public class Loan {
+    Date dateOfRegistration = new Date();
+    double sum;
+    double interestRate;
+    int loanTerm;
+    double monthlyPayment;
+
+}
