d9fc64c03d1a16f451523e21d9f33957b8625828
User <Nasty-Khropik>
2022-03-25 23:14:18 +0300

    Feature: добавили метод showBankMenu в классе BankMenu

--- src/com/company/week7/BankMenu.java
+++ src/com/company/week7/BankMenu.java
@@ -12,7 +12,7 @@ public class BankMenu {
     }
 
     public void showStartMenu() {
-        System.out.println("Select one:" + "\n1. Login" + "\n2. Register" + "\n0. Exit");
+        System.out.println("Select one: \n1. Login \n2. Register \n0. Exit");
         Scanner sc = new Scanner(System.in);
         int choose = sc.nextInt();
         if (choose == 1) {
@@ -22,5 +22,9 @@ public class BankMenu {
             choose.showRegister();
         }
         else (System.exit(0));
+    }
+
+    public void showBankMenu(){
+        System.out.println("1.Show my info \n2.Add loan \n3.Add debit card \n0.Exit");
     }
 }
