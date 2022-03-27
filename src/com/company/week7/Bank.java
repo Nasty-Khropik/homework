829225cdc71a83ffdffcb4eb68a9dbcbf65be825
AlehShyliuk <47559090+AlehShyliuk@users.noreply.github.com>
2022-03-27 12:02:30 +0400

    Feedback

--- src/com/company/week7/Bank.java
+++ src/com/company/week7/Bank.java
@@ -1,5 +1,6 @@
 package com.company.week7;
 
+//почему-то ты создала поле menu и методы doLogin/doRegister внутри метода start. Это все должно быть вне метода start, просто внутри класса Bank
 public class Bank {
     public void start(){
         BankMenu menu = new BankMenu();
